94e3b2eae3949920a7e3386b057d2297eb6edd3e
Logan <arechery.a1111@gmail.com>
2022-04-22 13:48:27 +0800

    Remove AntenaPort.cs using

--- RB/RebuildCubic/RebuildCubic/JWReaderFolder/AntenaPort.cs
+++ RB/RebuildCubic/RebuildCubic/JWReaderFolder/AntenaPort.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RebuildCubic.JWReaderFolder {
+﻿namespace RebuildCubic.JWReaderFolder {
   // JW.UHF.AntennaPort
   public class AntennaPort {
     public int AntennaIndex { get; set; }
